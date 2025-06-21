47a63add4bde39d6f6469bad6357b83c9d35ee8b
Miguel Sosa <msosadesign@gmail.com>
2025-06-21 17:28:03 +0200

    Make basic responsive layout

--- app/components/MainNavigation.tsx
+++ app/components/MainNavigation.tsx
@@ -3,8 +3,8 @@ import NavigationItem from "./NavigationItem";
 
 function MainNavigation() {
     return (
-        <nav className="bg-gray-900 p-4 fixed bottom-0 inset-x-0 ">
-            <ul className="flex items-center justify-center gap-6">
+        <nav className="bg-gray-800 fixed bottom-0 inset-x-0  md:static border-t border-gray-700 md:border-t-0 md:border-r">
+            <ul className="flex items-center justify-center gap-6 md:justify-start md:gap-8 md:flex-col md:items-start md:sticky md:top-0 p-4">
                 <NavigationItem to="/" icon={House} />
                 <NavigationItem to="/search" icon={SearchIcon} />
                 <NavigationItem to="/artist" icon={Guitar} autoDissapear={true} />
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,7 +2,7 @@ import type { Route } from "./+types/home";
 import MainNavigation from "~/components/MainNavigation";
 import { Outlet } from "react-router";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -10,10 +10,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Dashboard() {
-  return <>
-  <MainNavigation />
-  <main className="pb-24">
-    <Outlet />
-  </main>
-  </>;
+  return <main className="flex min-h-screen bg-gray-900 text-white">
+    <MainNavigation />
+    <div className="pb-24">
+      <Outlet />
+    </div>
+  </main>;
 }
