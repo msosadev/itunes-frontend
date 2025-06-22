61063dcd5c730693af0527c1514a252fdd225037
Miguel Sosa <msosadesign@gmail.com>
2025-06-22 23:11:11 +0200

    Updates

---
+++ assets/PageError-BSTr7m6V.js
@@ -0,0 +1,6 @@
+import{o as e}from"./chunk-NL6KNZEE-Dc8vaAUU.js";import{c as a}from"./createLucideIcon-C0lJV8vF.js";/**
+ * @license lucide-react v0.518.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M12 17h.01",key:"p32p05"}],["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z",key:"1mlx9k"}],["path",{d:"M9.1 9a3 3 0 0 1 5.82 1c0 2-3 3-3 3",key:"mhlwft"}]],c=a("file-question-mark",s);function l({title:r,description:t}){return e.jsxs("div",{className:"wrapper h-full flex gap-1.5 items-center justify-center flex-col text-center",children:[e.jsx(c,{}),e.jsx("h1",{className:"text-2xl font-bold",children:r||"Unexpected error"}),t?e.jsx("p",{className:"text-gray-500",children:t}):"",e.jsx("a",{href:"/",children:"Home"})]})}export{l as P};
---
+++ assets/search-12kafhEf.js
@@ -0,0 +1,11 @@
+import{c as a}from"./createLucideIcon-C0lJV8vF.js";/**
+ * @license lucide-react v0.518.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m11.9 12.1 4.514-4.514",key:"109xqo"}],["path",{d:"M20.1 2.3a1 1 0 0 0-1.4 0l-1.114 1.114A2 2 0 0 0 17 4.828v1.344a2 2 0 0 1-.586 1.414A2 2 0 0 1 17.828 7h1.344a2 2 0 0 0 1.414-.586L21.7 5.3a1 1 0 0 0 0-1.4z",key:"txyc8t"}],["path",{d:"m6 16 2 2",key:"16qmzd"}],["path",{d:"M8.23 9.85A3 3 0 0 1 11 8a5 5 0 0 1 5 5 3 3 0 0 1-1.85 2.77l-.92.38A2 2 0 0 0 12 18a4 4 0 0 1-4 4 6 6 0 0 1-6-6 4 4 0 0 1 4-4 2 2 0 0 0 1.85-1.23z",key:"1de1vg"}]],o=a("guitar",c);/**
+ * @license lucide-react v0.518.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]],r=a("search",e);export{o as G,r as S};
