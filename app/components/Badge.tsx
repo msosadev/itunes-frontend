65ab6f5c0a7395e6c28ffa7a35e3eb2f3234b640
Miguel Sosa <msosadesign@gmail.com>
2025-06-22 17:51:44 +0200

    Update gradients with dedicated class

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -1,6 +1,6 @@
 export default function Badge({ label }: { label: string }) {
     return (
-        <div className="inline-block bg-gradient-to-r from-primary to-secondary text-white text-xs font-semibold p-0.5 rounded-full">
+        <div className="inline-block magic-gradient text-white text-xs font-semibold p-0.5 rounded-full">
             <span className="bg-gray-900 font-bold rounded-full px-2 p-1 block">{label}</span>
         </div>
     );
--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -2,7 +2,7 @@ export default function SectionTitle({ title }: { title: string }) {
     return (
         <div className="text-2xl font-bold mb-4 mt-6 space-y-2">
             <h2>{title}</h2>
-            <div className="bg-gradient-to-r from-primary to-secondary h-0.5"></div>
+            <div className="magic-gradient h-0.5"></div>
         </div>
     );
 }
